1fa86e7237d296bb1638555390bbf6d309b737bf
aleksandraswj <141584484+aleksandraswj@users.noreply.github.com>
2023-10-30 23:01:34 +0100

    Fix 2_2_4

--- Prog2_2_4/main.cpp
+++ Prog2_2_4/main.cpp
@@ -5,15 +5,21 @@ using namespace std;
 //2.2.4 Napisz funkcjê, która dostaje jako argument nieujemn¹ liczbê ca³kowit¹
 //n i zwraca jako wartoœæ liczbê 2n.
 
-unsigned int razyDwa (unsigned int n)
+unsigned int f2don (unsigned int n)
 {
-    return 2*n;
+    unsigned int wynik = 1;
+    for(unsigned int i=0; i<n; i++)
+    {
+        wynik *= 2;
+    }
+    return wynik;
+    //return 1 << n;
 }
 
 int main()
 {
     unsigned int n;
     cin >> n;
-    cout << razyDwa(n) << endl;
+    cout << f2don(n) << endl;
     return 0;
 }
