59719bf717aeef29292030215800f166a34d104b
aleksandraswj <141584484+aleksandraswj@users.noreply.github.com>
2023-10-30 23:09:43 +0100

    Fix 2_2_5

--- Prog2_2_5/main.cpp
+++ Prog2_2_5/main.cpp
@@ -5,15 +5,29 @@ using namespace std;
 //2.2.5 Napisz funkcjê, która dostaje jako argument liczbê ca³kowit¹
 //n i zwraca jako wartoœæ liczbê 2n.
 
-int razyDwa (int n)
+double f2don (int n)
 {
-    return 2*n;
+    double wynik = 1;
+    if (n>=0)
+    {
+        for(int i=0; i<n; i++)
+        {
+            wynik *= 2;
+        }
+    } else
+    {
+        for(int i=0; i<-n; i++)
+        {
+            wynik/=2;
+        }
+    }
+    return wynik;
 }
 
 int main()
 {
     int n;
     cin >> n;
-    cout << razyDwa(n) << endl;
+    cout << f2don(n) << endl;
     return 0;
 }
