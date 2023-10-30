41a51815e4f932d3be2349c842c975b6e88321bb
aleksandraswj <141584484+aleksandraswj@users.noreply.github.com>
2023-10-30 23:14:01 +0100

    Fix 2_2_6

--- Prog2_2_6/main.cpp
+++ Prog2_2_6/main.cpp
@@ -6,9 +6,15 @@ using namespace std;
 //n i m, z których co najmniej jedna jest ró¿na od zera i zwraca
 //jako wartoœæ nm.
 
-unsigned int mnozenie (unsigned int n, unsigned int m)
+
+unsigned int fndom (unsigned int n, unsigned int m)
 {
-   return m*n;
+    unsigned int wynik = 1;
+    for(unsigned int i=0; i<m; i++)
+    {
+        wynik *= n;
+    }
+    return wynik;
 }
 
 int main()
@@ -16,6 +22,6 @@ int main()
     unsigned int n;
     unsigned int m;
     cin >> n >> m;
-    cout << mnozenie (n, m) << endl;
+    cout << fndom(n, m) << endl;
     return 0;
 }
